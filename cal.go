64f446b6798e89588c1acff9fd7a2ad80962dcca
moon1goblin <goblinfromdahmoon@gmail.com>
2025-07-15 22:24:11 +0300

    add weekdays and date at the top xdxd

--- cal.go
+++ cal.go
@@ -10,8 +10,26 @@ import (
 
 // -------------------------------------------
 
-const calwidth uint = 7
-const calheight uint = 5
+const calwidth_g uint = 7
+const calheight_g uint = 5
+
+// couldnt make this constant but whatever
+var weekdays_rendered = func() string {
+	var weekdays_g = []string{
+		"Mon",
+		"Tue",
+		"Wed",
+		"Thu",
+		"Fri",
+		"Sat",
+		"Sun",
+	}
+	for index, weekday := range(weekdays_g) {
+		// add some padding, want 12 wide, 12-3 / 2 = 4 and 5
+		weekdays_g[index] = "     " + weekday + "      "
+	}
+	return lg.JoinHorizontal(lg.Left, weekdays_g...)
+}
 
 type daycell struct {
 	date uint
@@ -29,6 +47,7 @@ var dayCellNormalModelStyle = lg.NewStyle().
 
 var dayCellFocusedModelStyle = lg.NewStyle().
 		Inherit(dayCellNormalModelStyle).
+		BorderStyle(lg.ThickBorder()).
 		BorderForeground(lg.Color("69"))
 
 var dayCellTodayModelStyle = lg.NewStyle().
@@ -40,8 +59,11 @@ var dayCellTodayModelStyle = lg.NewStyle().
 type model struct {
 	cur_time time.Time
 	cur_weekday uint // monday 0 sunday 6
+
 	focused_day_x uint
 	focused_day_y uint
+	focused_day_time time.Time
+
 	upper_left_day time.Time
 	loaded_daycells []daycell
 }
@@ -62,6 +84,7 @@ func createModel() *model {
 	m.upper_left_day = m.cur_time.AddDate(0, 0, - (7 + int(m.cur_weekday)))
 	m.focused_day_x = m.cur_weekday
 	m.focused_day_y = 1
+	m.focused_day_time = m.cur_time
 
 	// // load days 2 month before and after, if scroll far enough ask for more
 	// for i := range calheight * calwidth {
@@ -77,21 +100,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 		case tea.KeyMsg:
 			switch msg.String() {
-				case "ctrl+c":
+				case "ctrl+c", "q":
 					return m, tea.Quit
-				case "l":
-					if m.focused_day_x < calwidth - 1 {
-						m.focused_day_x++
-					}
 				case "h":
 					if m.focused_day_x > 0 {
 						m.focused_day_x--
+						m.focused_day_time = m.focused_day_time.AddDate(0, 0, -1)
 					}
-				case "j":
-					if m.focused_day_y == calheight - 1 {
-						m.upper_left_day = m.upper_left_day.AddDate(0, 0, 7)
-					} else if m.focused_day_y < calheight - 1 {
-						m.focused_day_y++
+				case "l":
+					if m.focused_day_x < calwidth_g - 1 {
+						m.focused_day_x++
+						m.focused_day_time = m.focused_day_time.AddDate(0, 0, 1)
 					}
 				case "k":
 					if m.focused_day_y == 0 {
@@ -99,21 +118,34 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					} else if m.focused_day_y > 0 {
 						m.focused_day_y--
 					}
+					m.focused_day_time = m.focused_day_time.AddDate(0, 0, -7)
+				case "j":
+					if m.focused_day_y == calheight_g - 1 {
+						m.upper_left_day = m.upper_left_day.AddDate(0, 0, 7)
+					} else if m.focused_day_y < calheight_g - 1 {
+						m.focused_day_y++
+					}
+					m.focused_day_time = m.focused_day_time.AddDate(0, 0, 7)
 		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	month := make([]string, calheight)
-	week := make([]string, calwidth)
+	date_at_the_top_rendered := 
+		"                                          " +
+		m.focused_day_time.Format("Mon Jan 2 2006")
+	alltogethernow := []string{date_at_the_top_rendered, "", weekdays_rendered()}
+
+	month := make([]string, calheight_g)
+	week := make([]string, calwidth_g)
 
 	cur_time := m.upper_left_day
-	for cury := range calheight {
-		for curx := range calwidth {
+	for cury := range calheight_g {
+		for curx := range calwidth_g {
 			if m.focused_day_x == curx && m.focused_day_y == cury {
 				week[curx] = dayCellFocusedModelStyle.Render(strconv.Itoa(cur_time.Day()))
-					// strconv.Itoa(cur_time.Day())
+				// strconv.Itoa(cur_time.Day())
 			} else if cur_time.Equal(m.cur_time) {
 				week[curx] = dayCellTodayModelStyle.Render(strconv.Itoa(cur_time.Day()))
 			} else {
@@ -123,7 +155,9 @@ func (m model) View() string {
 		}
 		month[cury] = lg.JoinHorizontal(lg.Left, week...)
 	}
-	return lg.JoinVertical(lg.Left, month...)
+
+	alltogethernow = append(alltogethernow, month...)
+	return lg.JoinVertical(lg.Left, alltogethernow...)
 }
 
 // ---------------------------------------------
